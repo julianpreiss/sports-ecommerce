e5f7a8f5857eb53d7b4d9a7a6832bcb936304317
Julian Preiss <julianpreiss23@gmail.com>
2022-02-23 21:29:22 -0300

    [micarrito.blade] correccion de estilos

--- resources/views/carrito/micarrito.blade.php
+++ resources/views/carrito/micarrito.blade.php
@@ -25,64 +25,64 @@
 			<a class="d-inline-block btn btn-primary" href="{{ route('carrito.checkout') }}">Pagar</a>
 		</div>
 				
-			</div>
-		</div>
-		<div class="row d-flex justify-content-center">
-			<h3>Productos en carrito</h3>
-		</div>
-        <?php 
-        // print_r($idsproducto);
-        // print_r($matchingproductunities);
-        // print_r($productosarray);
-        // print_r($misproductos);
-        foreach($productosarray as $producto):
-        ?>
-			<article class="col-md-3 py-3 d-sm-block justify-content-center d-flex">
-				<div class="card ">
-					@if($producto['imagen'] !== null)
-					<img class="card-img-top img-carrito" src="{{ url('img/' . $producto['imagen']) }}" 
-						@if($producto['descripcionimg'] !== null)
-						alt="{{ $producto['descripcionimg']}}">
-						@else
-						alt="{{ $producto['nombre']}}">
-						@endif
+	</div>
+	</div>
+	<div class="row d-flex justify-content-center">
+		<h3 class="col-12">Productos en carrito</h3>
+
+	<?php 
+	// print_r($idsproducto);
+	// print_r($matchingproductunities);
+	// print_r($productosarray);
+	// print_r($misproductos);
+	foreach($productosarray as $producto):
+	?>
+		<article class="col-md-3 py-3 d-sm-block justify-content-center d-flex">
+			<div class="card">
+				@if($producto['imagen'] !== null)
+				<img class="card-img-top img-carrito" src="{{ url('img/' . $producto['imagen']) }}" 
+					@if($producto['descripcionimg'] !== null)
+					alt="{{ $producto['descripcionimg']}}">
+					@else
+					alt="{{ $producto['nombre']}}">
 					@endif
-					<div class="card-body">
-						<h3 class="card-title">{{$producto['nombre']}}</h3>
-					</div>
-					<ul class="list-group list-group-flush">
-						<li class="list-group-item">Precio: ARS ${{$producto['precio']}}</li>
-						<li class="list-group-item">
-							<div>Unidades en carrito: {{$producto['unidades']}}</div>
-							<div>
-								<div class="d-flex justify-content-center">
-									<form action="{{ route('carrito.eliminarunidad', ['idproducto' => $producto['id']]) }}" method="POST" enctype="multipart/form-data">
-										@csrf
-										<button type="submit" class="btn btn-secondary mt-1 mr-3">-</button> 
-									</form>
-									<form action="{{ route('carrito.sumarunidad', ['idproducto' => $producto['id']]) }}" method="POST" enctype="multipart/form-data">
-										@csrf
-										<button type="submit" class="btn btn-secondary mt-1">+</button> 
-									</form>
-								</div>
-							</div>
-						</li>
-					</ul>
-					<div class="card-body">
-						<a href="{{ route('detallesproducto', ['id' => $producto['id']]) }}" class="btn btn-primary">Ver Producto</a>
-
-					<form action="{{ route('carrito.eliminarproducto', ['idproducto' => $producto['id']]) }}" method="POST" enctype="multipart/form-data">
-						@csrf
-						<button type="submit" class="btn btn-danger mt-1">Eliminar del carrito</button> 
-					</form>
-					</div>
+				@endif
+				<div class="card-body">
+					<h3 class="card-title">{{$producto['nombre']}}</h3>
 				</div>
-			</article>
+				<ul class="list-group list-group-flush">
+					<li class="list-group-item">Precio: ARS ${{$producto['precio']}}</li>
+					<li class="list-group-item">
+						<div>Unidades en carrito: {{$producto['unidades']}}</div>
+						<div>
+							<div class="d-flex justify-content-center">
+								<form action="{{ route('carrito.eliminarunidad', ['idproducto' => $producto['id']]) }}" method="POST" enctype="multipart/form-data">
+									@csrf
+									<button type="submit" class="btn btn-secondary mt-1 mr-3">-</button> 
+								</form>
+								<form action="{{ route('carrito.sumarunidad', ['idproducto' => $producto['id']]) }}" method="POST" enctype="multipart/form-data">
+									@csrf
+									<button type="submit" class="btn btn-secondary mt-1">+</button> 
+								</form>
+							</div>
+						</div>
+					</li>
+				</ul>
+				<div class="card-body">
+					<a href="{{ route('detallesproducto', ['id' => $producto['id']]) }}" class="btn btn-primary">Ver Producto</a>
 
-            <?php
-            endforeach;
-		}
-            ?>
+				<form action="{{ route('carrito.eliminarproducto', ['idproducto' => $producto['id']]) }}" method="POST" enctype="multipart/form-data">
+					@csrf
+					<button type="submit" class="btn btn-danger mt-1">Eliminar del carrito</button> 
+				</form>
+				</div>
+			</div>
+		</article>
+
+		<?php
+		endforeach;
+	}
+		?>
 	</div>
 	<p class="col-12 text-center">Aún no tienes productos agregados al carrito. Visita nuestra galería de productos y empieza a llenar tu carrito!</p>
 </section>
