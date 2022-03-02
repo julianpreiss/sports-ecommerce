b3045b7f9475605e2e80181ae1bddff2de0604b4
Julian Preiss <julianpreiss23@gmail.com>
2022-03-02 01:56:19 -0300

    [PerfilController] trae datos para mostrar perfil

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -4,14 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Usuario;
+use App\Models\Producto;
+use App\Models\Pago;
 
 class PerfilController extends Controller
 {
     public function miperfil() {
 
-        $usuario = Usuario::select('email', 'rol_id')->where('id','=',(auth()->user()->id))->first()->toArray();
+        $usuario = Usuario::select('email', 'nombre', 'apellido')->where('id','=',(auth()->user()->id))->first()->toArray();
+        $productoscomprados = Pago::select('idproducto', 'unidades')->where('idusuario','=',(auth()->user()->id))->get()->toArray();
+        // $productos = [];
+        foreach($productoscomprados as &$producto) {
+            $unidades = $producto['unidades'];
+            $producto = Producto::select()->where('id','=',$producto['idproducto'])->first()->toArray();
+            $producto['unidades'] = $unidades;
+            // array_push($productos, $producto);
+        }
 
         return view('miperfil', [
+        // 'productos' => $productos,
+        'productoscomprados' => $productoscomprados,
         'usuario' => $usuario,
         ]);
     }
