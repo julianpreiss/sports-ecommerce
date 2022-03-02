198425dc3bbcd2a80f80894fedfcc6840136622c
Julian Preiss <julianpreiss23@gmail.com>
2022-03-02 01:58:24 -0300

    [UsuariosSeeder] agrega mas usuarios

--- database/seeders/UsuariosSeeder.php
+++ database/seeders/UsuariosSeeder.php
@@ -19,6 +19,8 @@ class UsuariosSeeder extends Seeder
             [
                 'email' => 'julipreiss@hotmail.com',
                 'password' => Hash::make('1234'),
+                'nombre' =>'Julian',
+                'apellido' => 'Preiss',
                 'rol_id' => 1,
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s'),               
@@ -29,6 +31,31 @@ class UsuariosSeeder extends Seeder
             [
                 'email' => 'juli@juli.com',
                 'password' => Hash::make('asdf'),
+                'nombre' =>'Julian',
+                'apellido' => 'Gonzales',
+                'rol_id' => 2,
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),               
+            ]
+            ]);    
+
+        DB::table('usuarios')->insert([
+            [
+                'email' => 'tomas@tomas.com',
+                'password' => Hash::make('asdf1234'),
+                'nombre' =>'Tomacito',
+                'apellido' => 'De Nevares',
+                'rol_id' => 2,
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),               
+            ]
+            ]);
+        DB::table('usuarios')->insert([
+            [
+                'email' => 'cris@gmail.com',
+                'password' => Hash::make('qwerty'),
+                'nombre' =>'Cristian',
+                'apellido' => 'Calderón',
                 'rol_id' => 2,
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s'),               
