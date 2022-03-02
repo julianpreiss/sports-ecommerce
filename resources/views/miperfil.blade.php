b0c5a824165d6586c168288799266baff0b5b0bb
Julian Preiss <julianpreiss23@gmail.com>
2022-03-02 01:58:46 -0300

    [miperfil] muestra de datos en pagina

--- resources/views/miperfil.blade.php
+++ resources/views/miperfil.blade.php
@@ -5,12 +5,43 @@
 @section('content')
 
 <section class="text-center offset-md-1 col-md-10 container mt-3">
-    <h2 class="col-12 text-center">Mi Perfil</h2>
-    <?php
-    print_r($usuario);
-    ?>
-
-
+    <div class="row">
+        <div class="col-12 col-md-6 ">    
+            <h2 class="col-12 text-center">Mi Perfil</h2>
+            <?php
+            // print_r($usuario);
+            ?>
+            <ul>
+                <li class="list-group-item">Email de usuario: {{ $usuario['email'] }}</li>
+                <li class="list-group-item">Nombre: {{ $usuario['nombre'] }}</li>
+                <li class="list-group-item">Apellido: {{ $usuario['apellido'] }}</li>
+            </ul>
+        </div>
+        <div class="col-12 col-md-6 ">
+            <h4>Mis compras</h4>
+            <table class="table table-bordered table-striped">
+                <thead>
+                    <tr>
+                        <th>Producto</th>
+                        <th>Precio</th>
+                        <th>Unidades</th>
+                    </tr>
+                </thead>
+                <tbody><?php 
+                    $total = 0;
+                    foreach($productoscomprados as $producto) {
+                    $total = $total + $producto['precio']*$producto['unidades']
+                    ?>
+                    <tr>
+                        <td>{{ $producto['nombre'] }}</td>
+                        <td>{{ $producto['precio'] }}</td>
+                        <td>{{ $producto['unidades'] }}</td>
+                    </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </section>
 
 @endsection
